331419929a375012229f60817ec4700b88fdc794
Stig B. Dørmænen <stigbd@gmail.com>
2021-01-14 19:16:46 +0100

    fix: add CORS support

--- src/dcat_ap_no_validator_service/app.py
+++ src/dcat_ap_no_validator_service/app.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from aiohttp import web
+from aiohttp_middlewares import cors_middleware
 from dotenv import load_dotenv
 
 from .view import Ping, Ready, Shape, Shapes, Validator
@@ -10,10 +11,17 @@ from .view import Ping, Ready, Shape, Shapes, Validator
 load_dotenv()
 LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
 
+# CORS configuration
+CORS_ALLOW_ORIGINS = ["https://staging..fellesdatakatalog.digdir.no"]
+
 
 async def create_app() -> web.Application:
     """Create an web application."""
-    app = web.Application()
+    app = web.Application(
+        middlewares=[
+            cors_middleware(origins=CORS_ALLOW_ORIGINS, allow_credentials=True)
+        ]
+    )
     app.add_routes(
         [
             web.view("/ping", Ping),
@@ -23,6 +31,7 @@ async def create_app() -> web.Application:
             web.view("/shapes/{id}", Shape),
         ]
     )
+
     # logging configurataion:
     logging.basicConfig(level=LOGGING_LEVEL)
     return app
